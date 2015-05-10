518c54559bb1f3f9e074b96d308d8ec1eba9b3e0
Anson Kahng <ansonkahng@gmail.com>
2015-05-10 16:20:40 -0400

    ctd stuff

--- src/spring15/graphScaling.py
+++ src/spring15/graphScaling.py
@@ -9,6 +9,7 @@ import copy
 import csv
 import os
 import networkx as nx
+import operator
 
 # CONSTANTS
 # gamma_0 = float(sys.argv[1])
@@ -144,7 +145,7 @@ def modifyGraph(graph,k):
 def ageGraph(graph):
 	# set initial fraction d of nodes to nonfunctional
 	graph_adjdict_list = []
-	graph_nodedeps = copy.deepcopy(getadjdict(graph))  # update this to break nodes to graph
+	graph_nodedeps = copy.deepcopy(getAdjdict(graph))  # update this to break nodes to graph
 	graph_adjdict_list.append(graph_nodedeps)
 
 	initialnonfunc = random.sample(range(0,graph.n),int(graph.d*graph.n))
@@ -156,10 +157,6 @@ def ageGraph(graph):
 	for x in graph.nodes:
 		functionalnodes.append(x.func)
 	functionalnodeslist.append(functionalnodes)
-
-	# print graph_nodedeps
-	# print functionalnodes
-	# print graph_adjdict_list
 
 	# initial update of vitality
 	graph.vitality.append(float(sum(c.func for c in graph.nodes)) / graph.n)
@@ -232,32 +229,27 @@ def ageGraph(graph):
 			functionalnodes.append(x.func)
 		functionalnodeslist.append(functionalnodes)
 
-		# print 'nodedeps:', graph_nodedeps
-		# print 'functional nodes:', functionalnodes
-		# print 'adjdict list:', graph_adjdict_list[0]
-
 	return graph, graph_adjdict_list,functionalnodeslist
 
-def getadjdict(graph):
+def getAdjdict(graph):
 	adjdict = {}
 	for i,g in enumerate(graph.nodes):
 		adjdict[i] = g.dep
 	return adjdict
 
 
-def getadjdictplus(graph):
+def getAdjdictPlusCount(graph):
 	adjdict = {}
-	adjdictcount = []
+	adjdict_count = []
 	for i,g in enumerate(graph.nodes):
 		adjdict[i] = g.dep
-		adjdictcount.append(len(g.dep))
-	return adjdict, adjdictcount
+		adjdict_count.append(len(g.dep))
+	return adjdict, adjdict_count
 
-def plotadjdict(adjdict,livenodes,figname,ctd):
+def plotAdjdict(adjdict,livenodes,figname,ctd):
 	figname = './graphs/' + figname + '.pdf'
 	nodecolors = []
-	# print 'livenodes', livenodes
-	# print 'ctd', ctd
+
 	for i,x in enumerate(livenodes):
 		if x == 0:
 			nodecolors.append('r')
@@ -283,56 +275,189 @@ def plotadjdict(adjdict,livenodes,figname,ctd):
 
 	plt.savefig(figname)
 
-def visualizedict(graph):
-	adjdict = getadjdict(graph)
-	plotadjdict(adjdict)
 
-def processGraphTimeData(graph2_adjdict_list, graph2_functionalnodeslist, adjdictcount):
-	pctalivelist = []
-	closetodeathll = []
-	for j,al in enumerate(graph2_adjdict_list):
-		pctalivelist.append({})
+def plotCTD(adjdict,livenodes,figname,ctd):
+	figname = './graphs/' + figname + '.pdf'
+	nodecolors = []
+
+	for i,x in enumerate(livenodes):
+		if x == 0:
+			nodecolors.append('r')
+		else:
+			nodecolors.append('b')
+
+	# edit nodecolors to find nodes close to death
+	for x in ctd:
+		if x[1]:
+			nodecolors[x[0]] = 'g'
+
+	plt.figure(figsize=(12,12))
+	G=nx.DiGraph(adjdict)
+	for i,x in enumerate(livenodes):
+		if x == 0:
+			if len(G.edges(i)) > 0:
+				for e in G.edges(i):
+					G.remove_edge(*e)
+
+	# only plot close to death?
+	for i,x in enumerate(nodecolors):
+		if x != 'g':
+			for e in G.edges(i):
+				G.remove_edge(*e)
+
+	nx.draw(G,node_size=50,with_labels=True,font_size=8,node_color=nodecolors)
+
+	# plt.show()
+
+	plt.savefig(figname)
+
+def getFuncCTD(adjdict,adjdict_count):
+	ctr = 0
+	for k,v in adjdict.items():
+		if len(v) / float(adjdict_count[k]) > 0.5:
+			ctr += 1
+	return ctr
+
+def analyzeCTD(currentadjdict,adjdict_count,livenodes,ctd,idx):
+	currentadjdict = currentadjdict[idx]
+	livenodes = livenodes[idx]
+	ctd = ctd[idx]
+
+	ctd_dict = {}
+
+	for x in ctd:  # for each ctd node
+		if x[1]:  # if ctd
+			# make copy of adjdict
+			tmp = copy.deepcopy(currentadjdict)
+			orig_dict_count = getFuncCTD(tmp,adjdict_count)
+			broken_node = x[0] # node name
+			for k,v in tmp.items():
+				if broken_node in v:
+					v.remove(broken_node)
+
+			ctr = 1
+			# recursively break dependencies
+			while ctr > 0:
+				ctr = 0
+				# check if nodes are broken
+				for k,v in tmp.items():
+					pctalive = len(tmp[k])/float(adjdict_count[k])
+					if pctalive < 0.5:
+						for k1,v1 in tmp.items():
+							if k in v1:
+								ctr += 1
+								v1.remove(k)
+
+			# ctd node broken: number of nodes still alive
+			numfunc = getFuncCTD(tmp,adjdict_count)
+
+			ctd_dict[x[0]] = (orig_dict_count,numfunc)
+
+
+	return ctd_dict
+
+def analyzeLiveNodes(currentadjdict,adjdict_count,livenodes,idx):
+	currentadjdict = currentadjdict[idx]
+	livenodes = livenodes[idx]
+
+	ln_dict = {}
+
+	for i,x in enumerate(livenodes):  # for each ctd node
+		if x == 1:  # if ctd
+			# make copy of adjdict
+			tmp = copy.deepcopy(currentadjdict)
+			orig_dict_count = getFuncCTD(tmp,adjdict_count)
+			broken_node = i # node name
+			for k,v in tmp.items():
+				if broken_node in v:
+					v.remove(broken_node)
+
+			ctr = 1
+			# recursively break dependencies
+			while ctr > 0:
+				ctr = 0
+				# check if nodes are broken
+				for k,v in tmp.items():
+					pctalive = len(tmp[k])/float(adjdict_count[k])
+					if pctalive < 0.5:
+						for k1,v1 in tmp.items():
+							if k in v1:
+								ctr += 1
+								v1.remove(k)
+
+			# ctd node broken: number of nodes still alive
+			numfunc = getFuncCTD(tmp,adjdict_count)
+
+			ln_dict[i] = (orig_dict_count,numfunc)
+
+
+	return ln_dict
+
+
+def processGraphTimeData(adjdict_list, functional_nodes_list, adjdict_count):
+	pct_alive_list = []
+	ctd_lists = []
+	for j,al in enumerate(adjdict_list):
+		pct_alive_list.append({})
 		closetodeathlist = []
 		for i,k in enumerate(al):
 			# print al[k]
-			pctalive = len(al[k])/float(adjdictcount[i])
-			tmp = len(al[k]) - float(adjdictcount[i])/2
+			pctalive = len(al[k])/float(adjdict_count[i])
+			tmp = len(al[k]) - float(adjdict_count[i])/2
 			closetodeath = (tmp < 1 and tmp >= 0)
-			pctalivelist[-1][k] = pctalive
+			pct_alive_list[-1][k] = pctalive
 
-			if graph2_functionalnodeslist[j][i] == 1: # node alive:
+			if functional_nodes_list[j][i] == 1: # node alive:
 				closetodeathlist.append((i,closetodeath))
 
-		closetodeathll.append(closetodeathlist)
+		ctd_lists.append(closetodeathlist)
 
 	# get number of alive vertices at each step close to death
-	numclosetodeath = []
-	fracclosetodeath = []
-	for l in closetodeathll:
+	num_CTD = []
+	frac_CTD = []
+	for l in ctd_lists:
 		# get true vs false count
 		x = [int(a[1]) for a in l]
 		num_close_to_death = sum(x)
 		num_alive_at_this_point = len(l)
-		numclosetodeath.append((num_close_to_death,num_alive_at_this_point))
+		num_CTD.append((num_close_to_death,num_alive_at_this_point))
 		if num_alive_at_this_point != 0:
-			fracclosetodeath.append(float(num_close_to_death)/num_alive_at_this_point)
+			frac_CTD.append(float(num_close_to_death)/num_alive_at_this_point)
 		else:
-			fracclosetodeath.append('div by 0')
+			frac_CTD.append('div by 0')
 
-	return pctalivelist, closetodeathll, numclosetodeath, fracclosetodeath
+	return pct_alive_list, ctd_lists, num_CTD, frac_CTD
 
-def visualize(n,sf):
-	# graph visualization
-
+def visualizeGraph(n,sf):
+	# create and age graph
 	graph = createGraph(n,sf,0.0)
-	graph2, graph2_adjdict_list, graph2_functionalnodeslist = ageGraph(graph)
+	aged_graph, adjdict_list, functional_nodes_list = ageGraph(graph)
 
-	len_adjdict_list = len(graph2_adjdict_list)
+	# number of iterations
+	len_adjdict_list = len(adjdict_list)
 
-	adjdict, adjdictcount = getadjdictplus(graph)
+	# get adjacency dictionary and counts for each timestep
+	adjdict, adjdict_count = getAdjdictPlusCount(graph)
 
-	pctalivelist, closetodeathll, numclosetodeath, fracclosetodeath = processGraphTimeData(graph2_adjdict_list, graph2_functionalnodeslist, adjdictcount)
+	# get all other summary statistics 
+	pct_alive_list, ctd_lists, num_CTD, frac_CTD = processGraphTimeData(adjdict_list, functional_nodes_list, adjdict_count)
 
+	# # ctd statistics
+	# tmpx = analyzeCTD(adjdict_list,adjdict_count,functional_nodes_list,ctd_lists,len_adjdict_list-3)
+	# a = [a for (a,_) in tmpx.values()]
+	# b = [b for (_,b) in tmpx.values()]
+	# avg = sum(b)/float(len(b))
+	# print a[0], sorted(b)
+	# print 'avg', avg, a[0]
+	# print 'num to 0', b.count(0), len(b)
+	# print 'num to death'  # get number that drops everything below threshold
+
+	# # IMPLEMENT OTHER METRIC OF VITALITY
+	# # ALSO REWRITE CODE. THIS IS NASTY AF
+	# # print tmpx
+	# sorted_x = sorted(tmpx.items(), key=operator.itemgetter(1))
+	# print sorted_x
+	# sys.exit()
 
 	# write all data to file
 	with open('./graphs/datalog.txt','ab') as f:
@@ -340,7 +465,7 @@ def visualize(n,sf):
 		print>>f, n
 		print>>f,sf
 		print>>f, len_adjdict_list
-		print>>f, zip(numclosetodeath,fracclosetodeath)
+		print>>f, zip(num_CTD,frac_CTD)
 		print>>f, '======================================'
 	f.close()
 
@@ -348,21 +473,56 @@ def visualize(n,sf):
 	for y in xrange(1,5):
 		idx = len_adjdict_list - y
 
-		ctd = closetodeathll[idx]
+		ctd = ctd_lists[idx]
+
+		tmpx = analyzeLiveNodes(adjdict_list,adjdict_count,functional_nodes_list,idx)
+		a = [a for (a,_) in tmpx.values()]
+		b = [b for (_,b) in tmpx.values()]
+		try:
+			avg = sum(b)/float(len(b))
+		except:
+			avg = 'divide by 0'
+		try:
+			print a[0], sorted(b)
+			print 'avg', avg, a[0]
+		except:
+			print 'no a'
+		print 'num to 0', b.count(0), '/', len(b)
+		# print 'num to death'  # get number that drops everything below threshold
+
+		print '==========================================='
+
+		tmpx = analyzeCTD(adjdict_list,adjdict_count,functional_nodes_list,ctd_lists,idx)
+		a = [a for (a,_) in tmpx.values()]
+		b = [b for (_,b) in tmpx.values()]
+		try:
+			avg = sum(b)/float(len(b))
+		except:
+			avg = 'divide by 0'
+		try:
+			print a[0], sorted(b)
+			print 'avg', avg, a[0]
+		except:
+			print 'no a'
+		print 'num to 0', b.count(0), '/', len(b)
 
 		# plot graph with only valid edges
 		figname = str(n) + '_' + sf + '_' + str(idx) + '_' + str(len_adjdict_list) + '_valid'
-		plotadjdict(graph2_adjdict_list[idx],graph2_functionalnodeslist[idx],figname,ctd)
+		plotAdjdict(adjdict_list[idx],functional_nodes_list[idx],figname,ctd)
 
 		# plot graph with all edges from nodes that are still alive
 		figname = str(n) + '_' + sf + '_' + str(idx) + '_' + str(len_adjdict_list) + '_alive'
-		plotadjdict(adjdict,graph2_functionalnodeslist[idx],figname,ctd)
+		plotAdjdict(adjdict,functional_nodes_list[idx],figname,ctd)
+
+		# plot only close to death nodes
+		figname = str(n) + '_' + sf + '_' + str(idx) + '_' + str(len_adjdict_list) + '_ctd'
+		plotCTD(adjdict_list[idx],functional_nodes_list[idx],figname,ctd)
 
 def main():
 	n = 200
-	sf = 'sf'
+	sf = 'r'
 
-	visualize(n,sf)
+	visualizeGraph(n,sf)
 
 if __name__ == '__main__':
 	main()
